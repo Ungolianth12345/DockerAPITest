d24c0d53ebeef34e5c04b7f65711a1b1eb54a3c7
Ungolianth12345 <48559399+Ungolianth12345@users.noreply.github.com>
2024-04-10 21:53:40 +0100

    some things to go over in dock.py file

--- dock.py
+++ dock.py
@@ -1,3 +1,23 @@
 import docker
 client = docker.from_env()
 
+# check if docker is running, raise exception if false.
+# Or don't and just go through a bunch of files per step.
+
+"""
+Things one can do:
+list containers:
+client.containers.list()
+
+run containers:
+client.containers.run("ubuntu", "echo hello world")
+
+building containers:
+" docker build -t getting-started . " is the same as
+" client.images.build(path = "./", tag = "getting-started") "
+
+pulling:
+client.images.pull(fedora:39) doesn't work
+client.images.pull(platform="fedora:39") doesn't work
+client.images.pull("fedora:latest") DOES
+"""
